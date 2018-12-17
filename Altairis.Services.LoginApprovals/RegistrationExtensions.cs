f1597c1a3defa56c79eb32ef0e53a476ee7bb38c
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-12-17 13:27:33 +0100

    Added registration extensions.

---
+++ Altairis.Services.LoginApprovals/RegistrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Altairis.Services.LoginApprovals;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.DependencyInjection {
+    public static class AltairisServicesLoginApprovalsRegistrationExtensions {
+
+        public static IServiceCollection AddLoginApprovals(this IServiceCollection services) {
+            services.AddHttpContextAccessor();
+            services.AddSingleton<ILoginApprovalSessionStore>(new InMemoryLoginApprovalSessionStore());
+            services.AddSingleton<LoginApprovalManager>();
+            return services;
+        }
+
+        public static IServiceCollection AddLoginApprovals(this IServiceCollection services, ILoginApprovalSessionStore store) {
+            services.AddHttpContextAccessor();
+            services.AddSingleton<ILoginApprovalSessionStore>(store);
+            services.AddSingleton<LoginApprovalManager>();
+            return services;
+        }
+
+    }
+}
